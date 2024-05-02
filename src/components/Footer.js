cb02604476801937c43d3c2497a4e55f2ec36784
Mazen Imad Jaber <57973260+MazenImadJaber@users.noreply.github.com>
2024-05-02 21:07:18 +1000

    style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Footer() {
   return (
     <footer className="bg-light text-center text-lg-start">
-    <div className="text-center p-3">
-      <span>Email: mazenj1997@gmail.com</span> |{" "}
-      <span>Phone: +1234567890</span>
-      <br />© {new Date().getFullYear()}
-    </div>
-  </footer>
-  )
+      <Container>
+        <Row>
+          <Col className="text-center p-3">Email: mazenj1997@gmail.com</Col>
+          <Col className="text-center p-3">Phone: +1234567890</Col>
+        </Row>
+
+        <Row className="text-center p-3">
+          <Col className="text-center p-3">© {new Date().getFullYear()}</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
