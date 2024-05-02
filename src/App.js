e5b5c641f7f005159d7b093bd95518143ca0cc1e
Mazen Imad Jaber <57973260+MazenImadJaber@users.noreply.github.com>
2024-05-02 21:14:26 +1000

    Navigation bar refactor

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Resume from "./pages/Resume";
 import Portfolio from "./pages/Portfolio";
 import Home from "./pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavBarComp from "./components/NavBarComp";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import NavBarComp from './NavBarComp'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
 
 export default function Header() {
   return (
     <header>
-      <NavBarComp />
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href='/'>IFN666 - Mazen Imad Jaber </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/resume">Resume</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </header>
   )
 }
