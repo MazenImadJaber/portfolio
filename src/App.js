69aca475c988ae670c1329fc6b0275e527cd46dc
Mazen Imad Jaber <57973260+MazenImadJaber@users.noreply.github.com>
2024-05-02 18:19:51 +1000

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink } from 'react-router-dom'
 
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <nav>
+       <ul>
+         <li>
+         <NavLink to="/">Home</NavLink>
+         </li>
+         <li>
+         <NavLink to="/about">About</NavLink>
+         </li>
+         <li>
+         <NavLink to="/resume">Resume</NavLink>
+         </li>
+         <li>
+         <NavLink to="/portfolio">Portfolio</NavLink>
+         </li>
+       </ul>
+     </nav>
     </div>
+    <Routes>
+     {/* <Route path='/' element={<Home/>}/> */}
+     <Route path='/' />
+     <Route path='/about'/>
+     <Route path='/resume' />
+     <Route path='/portfolio'/>
+    </Routes>
+ </Router>
   );
 }
 
