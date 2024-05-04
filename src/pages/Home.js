c98ebba247a647d0c7e39078f4bffe23cd7c1917
Mazen Imad Jaber <57973260+MazenImadJaber@users.noreply.github.com>
2024-05-04 17:30:45 +1000

    Style home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import homePic from '../assets/1648047955977.jpg'
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Button} from 'react-bootstrap';
+import "./Home.css"
+import { useNavigate } from 'react-router-dom';
 export default function Home() {
+    const navigate = useNavigate();
+    const goToAboutPage=()=>{
+        navigate("/about")
+    }
     return (
-        <Container >
-            <Row>
-                <Col>
-                <h1>Welcome to My Portfolio</h1>
-                <p>Hello! My name is Mazen Imad Jaber, Welcome to my perosnal website and Portfolio!</p>
-                </Col>
-                <Image className='image' alt="Mazen" src={homePic} fluid/>
-            </Row>
-           
-            
-        </Container>
+    < Container fluid className="p-0 full-height">
+      <Row noGutters className="hero-section">
+        <Col className="hero-image" >
+          <div className="hero-content">
+            <h1>Welcome to My Portfolio</h1>
+            <p>Hello! My name is Mazen Imad Jaber, Welcome to my perosnal website and Portfolio!</p>
+            <Button variant="primary" onClick={()=> goToAboutPage()}>Learn More</Button>
+          </div>
+        </Col>
+      </Row>
+    </Container> 
     );
 };
 
